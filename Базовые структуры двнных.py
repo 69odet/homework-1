1a1295ba2c2ffb97d47c37ff077cb16673044d1a
69odet <odet.o@yandex.ru>
2024-05-21 22:16:20 +0300

    Add files via upload

---
+++ Базовые структуры двнных.py
@@ -0,0 +1,23 @@
+a = 'Lecsika'
+print(len(a)) # Задача "Длина слова"
+first = 4
+print(first)
+second = 6
+print(second)
+summa = first+second
+print(summa)
+diff = second-first
+print(diff) # Задача "Суммы и разности"
+first = 5
+second = 3
+third = 7
+mean = first+second+third/3
+print(mean) # Задача "Среднее арифметическое"
+first_string = 'Вторник'
+second_string = 'Понедельник'
+print(second_string,', ',first_string) # Задача "Простые строчки"
+a = 2
+b = 7
+c = 4
+f = (((a*b)-(a*c))**3)/2
+print(f) # Задача "Сложная формула"
